48f5636b4a707325cfc914d9107e95b9981d0381
PiotrKrosniak <p.krosniak@gmail.com>
2024-06-07 16:19:43 +0200

    fixed login/signup links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
     <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-      <a
+    <Link
         className="flex items-center"
-        href="/"
+        to="/"
       >
+      
         <img
           alt=""
           className="h-6 mr-3 sm:h-9"
@@ -17,22 +18,21 @@ const Navbar = () => {
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
           bloom.ai
         </span>
-      </a>
+      </Link>
       <div className="flex items-center lg:order-2">
         
-      <a
+      <Link
         className="text-white bg-gray-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-        href="/login"
+        to="/login"
       >
         Login
-      </a>
-
-      <a
+      </Link>
+      <Link
         className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
-        href="/signup"
+        to="/signup"
       >
         SignUp
-      </a>
+      </Link>
 
         <button
           aria-controls="mobile-menu-2"
