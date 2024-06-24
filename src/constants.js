dee98d1c4838967d0c6e0a956e2ee4c17e4a96bb
Umang Zala <umangzala2k@gmail.com>
2024-06-24 19:28:38 +0530

    updated contants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
-export const API = process.env.API_URL;
-export const AUTH_TOKEN = process.env.ACCESS_AUTH_TOKEN;
+export const API = process.env.REACT_APP_API_URL;
+export const AUTH_TOKEN = process.env.REACT_APP_ACCESS_AUTH_TOKEN;
 export const BEARER = "Bearer";
+
+console.log("API_URL", API);
